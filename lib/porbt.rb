cee4c12c3dfa35706200ef737fc488bb4884c7b1
Tate Johnson <tate@tatey.com>
2011-10-19 21:30:15 +1000

    ActionView::Template will always be defined

--- lib/porbt.rb
+++ lib/porbt.rb
@@ -5,6 +5,4 @@ require 'porbt/format/json'
 require 'porbt/format/xml'
 require 'porbt/handler'
 
-if defined? ActionView::Template
-  ActionView::Template.register_template_handler :rb, Porbt::Handler
-end
+ActionView::Template.register_template_handler :rb, Porbt::Handler
